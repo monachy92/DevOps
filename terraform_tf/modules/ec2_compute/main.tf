5b89ee3b2911c45fc12cf305a32bee16c2850496
monachy92 <monarchy4292@gmail.com>
2025-09-17 12:54:28 -0400

    Update main.tf

---
+++ terraform_tf/modules/ec2_compute/main.tf
@@ -0,0 +1,35 @@
+resource "aws_ecr_repository" "python-apps" {
+  name                 = "python-apps"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "latest*"
+    filter_type = "WILDCARD"
+  }
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "dev-*"
+    filter_type = "WILDCARD"
+  }
+}
+
+resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
